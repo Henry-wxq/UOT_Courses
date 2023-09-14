a9c5e9629bc466eeb7e0e7888aa8015339d75c0f
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-13 21:58:15 -0400

    [UOT_MATH] Move content to a subfolder

---
+++ UOT_MATH/MAT301/Course_Structure/MAT301.mm
@@ -0,0 +1,151 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="MAT301" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1693876455484" STYLE="oval">
+<font SIZE="22"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="14" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="PRELIMINARIES" POSITION="bottom_or_right" ID="ID_547911317" CREATED="1693876406013" MODIFIED="1693876655140" HGAP_QUANTITY="67.4 pt" VSHIFT_QUANTITY="-16.2 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="14"/>
+<node TEXT="Properties of the integers" ID="ID_262999976" CREATED="1693876951976" MODIFIED="1693876969584"/>
+<node TEXT="Equivalence relation" ID="ID_40143064" CREATED="1693876971202" MODIFIED="1693876990743"/>
+<node TEXT="Modular arithmetics" ID="ID_1251330391" CREATED="1693876992017" MODIFIED="1693877005467"/>
+</node>
+<node TEXT="GROUPS" POSITION="bottom_or_right" ID="ID_206986187" CREATED="1693876614886" MODIFIED="1693876718705" HGAP_QUANTITY="72.8 pt" VSHIFT_QUANTITY="-13.2 pt">
+<node TEXT="Group axioms" ID="ID_1243422777" CREATED="1693877009335" MODIFIED="1693877016188"/>
+<node TEXT="Examples of groups" ID="ID_1421738415" CREATED="1693877016189" MODIFIED="1693877026960">
+<node TEXT="Symmetries of a square" ID="ID_1441166819" CREATED="1693877032886" MODIFIED="1693877043053"/>
+<node TEXT="Dihedral groups D_n" ID="ID_753459820" CREATED="1693877043056" MODIFIED="1693878090476"/>
+<node TEXT="Z_n and U(n)" ID="ID_1461355786" CREATED="1693877049044" MODIFIED="1693877100550"/>
+<node TEXT="Function groups" ID="ID_1654109743" CREATED="1693877100552" MODIFIED="1693877124525"/>
+<node TEXT="Matrix groups" ID="ID_742700513" CREATED="1693877124526" MODIFIED="1693877129680"/>
+</node>
+</node>
+<node TEXT="FINITE GROUPS" POSITION="bottom_or_right" ID="ID_1607289429" CREATED="1693876697363" MODIFIED="1693876751989" HGAP_QUANTITY="69.8 pt" VSHIFT_QUANTITY="-11.4 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Order of a group" ID="ID_932210766" CREATED="1693877188004" MODIFIED="1693877215536"/>
+<node TEXT="Order of a group element" ID="ID_279530229" CREATED="1693877395686" MODIFIED="1693877488992"/>
+</node>
+<node TEXT="SUBGROUPS" POSITION="bottom_or_right" ID="ID_1220891676" CREATED="1693876730653" MODIFIED="1693877527586" HGAP_QUANTITY="77.6 pt" VSHIFT_QUANTITY="-6.6 pt">
+<edge COLOR="#00007c"/>
+<node TEXT="Definition of a subgroup" ID="ID_238618825" CREATED="1693877217375" MODIFIED="1693877224929"/>
+<node TEXT="Subgroup test" ID="ID_1175397604" CREATED="1693877224931" MODIFIED="1693877265383"/>
+<node TEXT="Center of a group" ID="ID_1128701830" CREATED="1693877741158" MODIFIED="1693877771122"/>
+<node TEXT="Centraliser of an element" ID="ID_1643749045" CREATED="1693877771123" MODIFIED="1693877801117"/>
+</node>
+<node TEXT="CYCLIC GROUPS" POSITION="bottom_or_right" ID="ID_830387496" CREATED="1693876753334" MODIFIED="1693877522398" HGAP_QUANTITY="75.8 pt" VSHIFT_QUANTITY="-9 pt">
+<edge COLOR="#007c00"/>
+<node TEXT="Definition of cyclic groups" ID="ID_1363431418" CREATED="1693877273321" MODIFIED="1693877286592"/>
+<node TEXT="The fundamental theorem of cyclic groups" ID="ID_500984801" CREATED="1693877288431" MODIFIED="1693877510733"/>
+</node>
+<node TEXT="PERMUTATIONS" POSITION="bottom_or_right" ID="ID_1800670311" CREATED="1693876762712" MODIFIED="1693876937929" HGAP_QUANTITY="74 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="Symmetric groups S_n" ID="ID_490240534" CREATED="1693877831600" MODIFIED="1693877978046"/>
+<node TEXT="Cycle notation" ID="ID_1905585670" CREATED="1693877841551" MODIFIED="1693877869379">
+<node TEXT="Calculate the product of cycles" ID="ID_1918332980" CREATED="1693877872797" MODIFIED="1693877890684"/>
+<node TEXT="Cycle factorisation theorem" ID="ID_151993636" CREATED="1693877890686" MODIFIED="1693877916466"/>
+</node>
+<node TEXT="Even and odd permutations" ID="ID_1126695522" CREATED="1693877917486" MODIFIED="1693877958567">
+<node TEXT="Alternating groups A_n" ID="ID_634428047" CREATED="1693877963135" MODIFIED="1693877983069"/>
+</node>
+</node>
+<node TEXT="HOMOMORPHISMS AND ISOMORPHISMS" POSITION="bottom_or_right" ID="ID_1904848993" CREATED="1693876772116" MODIFIED="1693876936819" HGAP_QUANTITY="75.2 pt" VSHIFT_QUANTITY="6 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="Definitions" ID="ID_344667132" CREATED="1693878091646" MODIFIED="1693878097648"/>
+<node TEXT="Properties of isomorphisms" ID="ID_1073375198" CREATED="1693879532777" MODIFIED="1693879544249"/>
+<node TEXT="Automorphism and the automorphism group of a group Aut(G)" ID="ID_679878041" CREATED="1693879480621" MODIFIED="1693879528518"/>
+</node>
+<node TEXT="COSETS" POSITION="bottom_or_right" ID="ID_1527419602" CREATED="1693876783607" MODIFIED="1693876939989" HGAP_QUANTITY="77 pt" VSHIFT_QUANTITY="5.4 pt">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Definition of cosets" ID="ID_1050267577" CREATED="1693879547818" MODIFIED="1693879556479"/>
+<node TEXT="Lagrange&apos;s theorem" ID="ID_622093364" CREATED="1693879556480" MODIFIED="1693879709679"/>
+<node TEXT="Group actions" ID="ID_357053917" CREATED="1693879728057" MODIFIED="1693879736254">
+<node TEXT="Orbits and stabilisers" ID="ID_351218645" CREATED="1693879736256" MODIFIED="1693879802410"/>
+<node TEXT="Orbit-stabiliser theorem" ID="ID_152351147" CREATED="1693879750404" MODIFIED="1693879806860"/>
+</node>
+</node>
+<node TEXT="DIRECT PRODUCTS" POSITION="bottom_or_right" ID="ID_696058044" CREATED="1693876791050" MODIFIED="1693876941642" HGAP_QUANTITY="78.2 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Definition of external direct product" ID="ID_1139100546" CREATED="1693879830638" MODIFIED="1693879960053"/>
+</node>
+<node TEXT="NORMAL SUBGROUPS AND FACTOR GROUPS" POSITION="bottom_or_right" ID="ID_1942317612" CREATED="1693876804630" MODIFIED="1693876943049" HGAP_QUANTITY="77.6 pt" VSHIFT_QUANTITY="5.4 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Definitions" ID="ID_1976838738" CREATED="1693879967850" MODIFIED="1693880007375"/>
+<node TEXT="Normal group test" ID="ID_274039306" CREATED="1693880007376" MODIFIED="1693880013891"/>
+<node TEXT="Kernel (of an homomorphism)" ID="ID_1904369535" CREATED="1693880013893" MODIFIED="1693880985408"/>
+<node TEXT="The first isomorphism theorem" ID="ID_1441734389" CREATED="1693880985410" MODIFIED="1693881018868"/>
+<node TEXT="Inner direct product" ID="ID_1089624217" CREATED="1693881021760" MODIFIED="1693881034402"/>
+</node>
+<node TEXT="FINITE ABELIAN GROUPS" POSITION="bottom_or_right" ID="ID_1700748563" CREATED="1693876820801" MODIFIED="1693876947386" HGAP_QUANTITY="81.8 pt" VSHIFT_QUANTITY="10.8 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="The fundamental theorem of finitely generated Abelian groups" ID="ID_858781075" CREATED="1693881062799" MODIFIED="1693881352812"/>
+<node TEXT="Classification of Abelian groups of order n" ID="ID_1122858512" CREATED="1693881809876" MODIFIED="1693881833098"/>
+</node>
+</node>
+</map>
